9625ffd9ad5bb2ec056aaa02da6a941e7fe34202
Jimmy Anderson Concepción Polanco <jimmy_concepcion@ucne.edu.do>
2025-01-17 22:59:19 -0400

    Carpeta DAL creada

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.Models;
+
+
+namespace RegistroTecnico.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
