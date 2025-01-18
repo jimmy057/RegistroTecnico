452d1c5bb26d45b72adac6df2c85590519a953ae
Jimmy Anderson Concepción Polanco <jimmy_concepcion@ucne.edu.do>
2025-01-17 22:57:18 -0400

    carpeta models creada

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Nombres { get; set; } = null!;
+
+        public int SueldoHora { get; set; }
+    }
+
+
+}
