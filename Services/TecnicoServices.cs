ce57c8a52a4f72abf438e2442f60eb7a42d17d9c
Jimmy Anderson Concepción Polanco <jimmy_concepcion@ucne.edu.do>
2025-01-17 23:26:57 -0400

    metodos del tecnico services creados

--- Services/TecnicoServices.cs
+++ Services/TecnicoServices.cs
@@ -1,7 +1,69 @@
-﻿namespace RegistroTecnico.Services
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnico.Services
 {
-    public class TecnicoServices
+    public class TecnicoServices(IDbContextFactory<Contexto> DbFactory)
     {
+        public async Task<bool> Existe(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tecnicos.AnyAsync(t => t.TecnicoId == id);
+        }
+
+        private async Task<bool> Insertar(Tecnicos tecnico)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Tecnicos.Add(tecnico);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Modificar(Tecnicos tecnico)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Tecnicos.Update(tecnico);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Guardar(Tecnicos tecnico)
+        {
+            if (!await Existe(tecnico.TecnicoId))
+                return await Insertar(tecnico);
+            else
+                return await Modificar(tecnico);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            var Tecnicos = await contexto.Tecnicos
+                .Where(t => t.TecnicoId == id).ExecuteDeleteAsync();
+            return Tecnicos > 0;
+        }
+
+        public async Task<Tecnicos?> Buscar(int id)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tecnicos.AsNoTracking().
+                FirstOrDefaultAsync(t => t.TecnicoId == id);
+        }
+
+        public async Task<Tecnicos?> BuscarNombres(string nombre)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tecnicos.AsNoTracking()
+                .FirstOrDefaultAsync(t => t.Nombres == nombre);
+        }
+
+        public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Tecnicos.AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
 
     }
 }
