3ed5607346a5ba52add2bd6a2ec1b59b8e97c9a9
jimmy057 <jimmypolanco2540@gmail.com>
2025-01-25 21:14:23 -0400

    inyectando clientes en el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,6 @@ namespace RegistroTecnico.DAL
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
         public DbSet<Tecnicos> Tecnicos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
     }
 }
