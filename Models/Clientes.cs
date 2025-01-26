7903de4cbc600400bc4bb1c41a7860039534608f
jimmy057 <jimmypolanco2540@gmail.com>
2025-01-25 22:43:33 -0400

    modificacion en el cliente

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -13,20 +13,19 @@ namespace RegistroTecnico.Models
         public DateTime FechaIngreso { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio.")]
-        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+        [StringLength(30, ErrorMessage = "El nombre no puede exceder los 30 caracteres.")]
         public string Nombres { get; set; }
 
         [StringLength(200, ErrorMessage = "La dirección no puede exceder los 200 caracteres.")]
         public string Direccion { get; set; }
 
-        [StringLength(11, ErrorMessage = "El RNC no puede exceder los 11 caracteres.")]
-        public string RNc { get; set; }
+        [StringLength(9, ErrorMessage = "El RNC no puede exceder los 9 caracteres.")]
+        public string RNC { get; set; }
 
         [Required(ErrorMessage = "El límite de crédito es obligatorio.")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal LimiteCredito { get; set; }
 
-        // Relación con Técnico
         [Required(ErrorMessage = "El Técnico asociado es obligatorio.")]
         public int TecnicoId { get; set; }
     }
