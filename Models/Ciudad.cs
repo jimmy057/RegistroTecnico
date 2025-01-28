70a7cefd5029d78e63e95d58f208453f372ffd7a
jimmy057 <jimmypolanco2540@gmail.com>
2025-01-28 00:31:57 -0400

    modelo ciudad creado

---
+++ Models/Ciudad.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    public class Ciudad
+    {
+        public int CiudadId { get; set; }
+        [Required(ErrorMessage = "El campo es obligatorio.")]
+        public string Name { get; set; }
+        public int TecnicoId { get; internal set; }
+    }
+}
