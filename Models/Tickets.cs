fa83d23cc8f13e00d3f1afb433a48619d8ac57a0
jimmy057 <jimmypolanco2540@gmail.com>
2025-02-01 17:52:08 -0400

    models creado

---
+++ Models/Tickets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnico.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketsId { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public  String Prioridad { get; set; }
+        [Required]
+        public int ClienteId { get; set; }
+        public  String Asunto { get; set; }
+        [Required]
+        public  string Descripcion { get; set; }
+        public Double TiempoInvertido { get; set; }
+        [Required]
+        public int TecnicoId { get; set; }
+
+    }
+}
