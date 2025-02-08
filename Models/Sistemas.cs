d7a048fa42045ce1bf9ab1e7058e1434678db87e
jimmy057 <jimmypolanco2540@gmail.com>
2025-02-08 16:32:34 -0400

    modelo creado

---
+++ Models/Sistemas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemasId { get; set; }
+       
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        public String Descripcion { get; set; }
+
+        [Range(1, 100, ErrorMessage = "La complejidad debe estar entre 1 y 100.")]
+        public int Complejidad { get; set; }
+        public int TecnicoId { get; set; }
+    }
+}
